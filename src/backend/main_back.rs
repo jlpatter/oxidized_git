d8cd9ec458ca3cc9af74b0c674f1815a84bf0a29
Joshua Patterson <joshua.l.patterson7@gmail.com>
2022-07-03 14:27:17 -0500

    Switch open_repo to return a Result

--- src/backend/main_back.rs
+++ src/backend/main_back.rs
@@ -1,11 +1,16 @@
 use git2::Repository;
+use git2::Error;
 use rfd::AsyncFileDialog;
 
-pub async fn open_repo() -> Option<Repository> {
-    let file_handle = AsyncFileDialog::new().set_directory("/").pick_folder().await?;
-    match Repository::open(file_handle.path()) {
-        Ok(repo) => Some(repo),
-        Err(e) => panic!("Repository not found at given path: {}", e),
+pub async fn open_repo() -> Result<Option<Repository>, Error> {
+    match AsyncFileDialog::new().set_directory("/").pick_folder().await {
+        Some(file_handle) => {
+            match Repository::open(file_handle.path()) {
+                Ok(repo) => Ok(Some(repo)),
+                Err(e) => Err(e),
+            }
+        },
+        None => Ok(None),
     }
 }
 
