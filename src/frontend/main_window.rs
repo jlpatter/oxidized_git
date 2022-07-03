c3fee4eba74fe36364ab3a23816cedb356d92e09
Joshua Patterson <joshua.l.patterson7@gmail.com>
2022-07-02 21:03:15 -0500

    Fix button layout and some error handling

--- src/frontend/main_window.rs
+++ src/frontend/main_window.rs
@@ -5,36 +5,40 @@ slint::slint! {
     import { Button } from "std-widgets.slint";
     OGFWindow := Window {
         title: "Orange Git Fish";
-        // callback fetchBtn-pressed <=> fetchBtn.clicked;
+        callback fetchBtn-pressed <=> fetchBtn.clicked;
         callback openBtn-pressed <=> openBtn.clicked;
-        openBtn := Button {
-            text: "Open";
+        GridLayout {
+            spacing: 5px;
+            Row {
+                openBtn := Button {
+                    text: "Open";
+                    clip: true;
+                }
+                fetchBtn := Button {
+                    text: "Fetch";
+                    clip: true;
+                }
+            }
         }
-        // fetchBtn := Button {
-        //     text: "Fetch";
-        // }
     }
 }
 
 pub fn main() {
     let ogf_window = OGFWindow::new();
-    // ogf_window.on_openBtn_pressed(move || {
-    //     main_back::open_repo();
-    // });
     ogf_window.on_openBtn_pressed(move || {
         let repo_opt = executor::block_on(main_back::open_repo());
         match repo_opt {
             Some(repo) => {
                 match repo.path().to_str() {
                     Some(path_str) => println!("{}", path_str),
-                    None => panic!("Path object didn't return a string!"),
+                    None => (),
                 }
             },
-            None => panic!("No repo returned!"),
+            None => (),
         }
     });
-    // ogf_window.on_fetchBtn_pressed(move || {
-    //     main_back::git_fetch();
-    // });
+    ogf_window.on_fetchBtn_pressed(move || {
+        main_back::git_fetch();
+    });
     ogf_window.run();
 }
