90b5390eba958b2b6927768b9fac1645c13ff5f7
Joshua Patterson <joshua.l.patterson7@gmail.com>
2022-07-03 10:42:26 -0500

    Setup proper multithreading

--- src/backend/main_back.rs
+++ src/backend/main_back.rs
@@ -1,15 +1,17 @@
 use git2::Repository;
 use rfd::AsyncFileDialog;
 
-pub async fn open_repo() -> Option<Repository> {
-    let file_handle = AsyncFileDialog::new()
-        .set_directory("/")
-        .pick_folder()
-        .await?;
-    let path = file_handle.path().to_str()?;
-    match Repository::open(path) {
-        Ok(some_repo) => Some(some_repo),
-        Err(e) => panic!("Repository not found at given path: {}", e),
+pub async fn open_repo() {
+    match AsyncFileDialog::new().set_directory("/").pick_folder().await {
+        Some(file_handle) => {
+            match Repository::open(file_handle.path()) {
+                Ok(repo) => {
+                    println!("Do something with repo!");
+                },
+                Err(e) => panic!("Repository not found at given path: {}", e),
+            }
+        },
+        None => (),
     }
 }
 
--- src/frontend/main_window.rs
+++ src/frontend/main_window.rs
@@ -1,4 +1,5 @@
-use futures::executor;
+use futures::executor::ThreadPool;
+use futures::task::SpawnExt;
 use crate::backend::main_back;
 
 slint::slint! {
@@ -27,17 +28,10 @@ slint::slint! {
 
 pub fn main() {
     let ogf_window = OGFWindow::new();
+    let pool = ThreadPool::new().unwrap();
     ogf_window.on_openBtn_pressed(move || {
-        let repo_opt = executor::block_on(main_back::open_repo());
-        match repo_opt {
-            Some(repo) => {
-                match repo.path().to_str() {
-                    Some(path_str) => println!("{}", path_str),
-                    None => println!("Repo Path String returned None."),
-                }
-            },
-            None => println!("Repo returned None."),
-        }
+        pool.spawn(main_back::open_repo()).expect("Thread failed to spawn!");
+        // executor::block_on(main_back::open_repo());
     });
     ogf_window.on_fetchBtn_pressed(move || {
         main_back::git_fetch();
