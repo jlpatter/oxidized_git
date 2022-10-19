5fb93333965b60101bef36f6e8d8bdb6642b0f4b
victor <victorvaldez5@gmail.com>
2022-10-19 12:28:11 -0400

    start making components

--- og-ui/src/App.vue
+++ og-ui/src/App.vue
@@ -2,10 +2,15 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import TreeView from './components/TreeView.vue'
 </script>
 
 <template>
-  <div>
+  <tree-view />
+  <div class="display-flex-column full-height">
+    <div class="display-flex-row">
+      
+    </div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
